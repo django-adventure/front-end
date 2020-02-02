caeb0df857e0ecaaa2848b76603710f5a4916375
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-02 13:55:42 -0500

    made axios post in Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,34 +1,56 @@
-import React, { useState } from 'react';
-import AuthWrapper from './auth.styled';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import AuthWrapper from "./auth.styled";
+import styled from "styled-components";
+import axios from "axios";
 
 function Login() {
-  const [user, setUser] = useState({ username: '', password: '' });
+  const [user, setUser] = useState({ username: "", password: "" });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
-  const handleError = (error) => {
+  const handleError = error => {
     setError(error);
     setLoading(false);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    setError(null);
     setLoading(true);
-    console.log(user);
+    axios
+      .post("http://localhost:8000/api/login/", user)
+      .then(res => {
+        console.log(res);
+        setLoading(false);
+        window.localStorage.setItem("token", res.data.key);
+      })
+      .catch(err => {
+        setError(err);
+        setLoading(false);
+      });
   };
 
   return (
     <AuthWrapper>
       <h1>Sign In</h1>
       <StyledForm onSubmit={handleSubmit}>
-        <input name="username" type="text" placeholder="Username" value={user.username} onChange={handleChange}/>
-        <input name="password" type="text" placeholder="Password" value={user.password} onChange={handleChange}/>
+        <input
+          name="username"
+          type="text"
+          placeholder="Username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="Password"
+          value={user.password}
+          onChange={handleChange}
+        />
         <button type="submit">Sign In</button>
       </StyledForm>
     </AuthWrapper>
