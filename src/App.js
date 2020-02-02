fbd3ad89f71ae35501a5dc40e8a6e19efa2973c3
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-02-02 11:21:01 -0600

    basic router setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React, { Fragment } from 'react';
-import './App.css';
+import { Route, Link } from 'react-router-dom';
 import GlobalStyle from './global-styles';
+import styled from 'styled-components';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
     <Fragment>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrapper>
+        <Header>
+          <StyledLink to="/">Untitled MUD Game</StyledLink>
+          <nav>
+            <StyledLink to="/login">Login</StyledLink>
+            <StyledLink to="/register">Register</StyledLink>
+          </nav>
+        </Header>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </AppWrapper>
       <GlobalStyle />
     </Fragment>
   );
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  font-size: calc(10px + 2vmin);
+  color: white;
+
+  a {
+    color: #61dafb;
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+
+  h1 {
+    font-size: 24px;
+    margin-top: 0;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  &:not(:first-child) {
+    margin-left: 15px;
+  }
+`;
