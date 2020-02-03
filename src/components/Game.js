158cb73a69efa6bfe8f49eb1044de3594d3b5b00
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-02-03 15:39:02 -0600

    showing room info

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-
-export default function() {
-  return <h1>Game</h1>;
+function RoomInfo({ user, currentRoom }) {
+  return (
+    <div>
+      Username: {user} <br />
+      Room: {currentRoom.title} <br />
+      Description: {currentRoom.description} <br />
+      Players: {currentRoom.players.join(', ')}
+    </div>
+  );
 }
+
+function Game() {
+  const [user, setUser] = useState('');
+  const [currentRoom, setCurrentRoom] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('api/adv/init/')
+      .then((res) => {
+        console.log(res.data);
+
+        const { name, title, description, players } = res.data;
+        setUser(name);
+        setCurrentRoom({ title, description, players });
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return loading ? null : (
+    <Fragment>
+      <RoomInfo currentRoom={currentRoom} user={user} />
+    </Fragment>
+  );
+}
+
+export default Game;
